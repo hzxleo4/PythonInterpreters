1d4d1fef82ca4b16292bf32bedb924026fa34336
hzxleo4 <hzxleo4@163.com>
2020-01-30 18:03:07 +0800

    nested assign available

--- Interpreters.py
+++ Interpreters.py
@@ -1,26 +1,47 @@
 Variables = {} 
-def Assign(Lines):
-	LeftExp,RightExp = Lines.split("=")
-	LeftExp = LeftExp.rstrip()
+def ChangeVariables(Expression):
 	Variableslist =  sorted([key for key in Variables],key = lambda x:len(x),reverse = True)
 	for key in Variableslist:
-		if str(key) in RightExp:
+		if str(key) in Expression:
 			if type(Variables[key]) == str:
-				RightExp =  RightExp.replace(str(key),'\''+str(Variables[key]+'\''))
+				Expression =  Expression.replace(str(key),'\''+str(Variables[key]+'\''))
 			else:
-				RightExp =  RightExp.replace(str(key),str(Variables[key]))
+				Expression =  Expression.replace(str(key),str(Variables[key]))
+	return Expression
+def Assign(Lines):
+	LeftExp,RightExp = Lines.split("=")
+	LeftExp = LeftExp.strip()
+	RightExp = ChangeVariables(RightExp)
 	LeftExp = LeftExp.split(",")
 	LeftExp = [Var.strip() for Var in LeftExp]
 	RightExp = eval(RightExp)
 	#print(type(LeftExp),len(LeftExp))
 	#print(RightExp)
-	try:
-		for i in range(len(LeftExp)):
-			Variables[LeftExp[i]] = RightExp[i]
-	except TypeError:
-		#print("wdwd",LeftExp,RightExp)
-		Variables[LeftExp[0]] = RightExp
+	#print(LeftExp)
+	for i in range(len(LeftExp)):
+		if '[' in LeftExp[i]:
+			Name = LeftExp[i][:LeftExp[i].find("[")]
+			Name = Variables[Name]
+			Key = LeftExp[i][LeftExp[i].find('[')+1:LeftExp[i].find(']')]
+			Key = int(Key) if Key.isdigit() else Key 
+			LeftExp[i] = LeftExp[i][LeftExp[i].find("]")+1:]
+			while LeftExp[i].count("[") != 0:
+				Name = Name[Key]
+				Key = LeftExp[i][LeftExp[i].find('[')+1:LeftExp[i].find(']')]
+				Key = int(Key) if Key.isdigit() else Key 
+				LeftExp[i] = LeftExp[i][LeftExp[i].find("]")+1:]
+			if len(LeftExp) == 1:
+				Name[Key] = RightExp
+			else:
+				Name[Key] = RightExp
+		else:
+			if len(LeftExp) == 1:
+				Variables[LeftExp[i]] = RightExp
+			else:
+				Variables[LeftExp[i]] = RightExp[i]
 
+#Lines = "a = [1,2,[3,4]]"
+#print(">>>",end='')
 while True:
 	Lines = input(">>>")
 	if Lines == "exit()":
@@ -29,6 +50,10 @@ while True:
 		Assign(Lines)
 	elif Lines in Variables:
 		print("A",Variables[Lines])
+	elif '[' in Lines:
+		Lines = ChangeVariables(Lines)
+		print(eval(Lines))
 	else:
 		print("B",eval(Lines))
 	print(Variables)
+	#Lines = input(">>>")
