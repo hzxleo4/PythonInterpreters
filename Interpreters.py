eb704dcde716b3cbaa3a4a60dc374cadab226b3a
hzxleo4 <hzxleo4@163.com>
2020-01-31 16:33:49 +0800

    simple if-statement available

--- Interpreters.py
+++ Interpreters.py
@@ -39,14 +39,39 @@ def Assign(Lines):
 				Variables[LeftExp[i]] = RightExp
 			else:
 				Variables[LeftExp[i]] = RightExp[i]
+def Branch(Lines):
+	Stack = []
+	while True:
+		Stack.append(Lines)
+		Lines = input('...')
+		#print("-->",Lines)
+		try:
+			if Lines[:4] != "    " and Lines[:4] != 'elif' and Lines[:5] != 'else:' and Lines[0] !='\t':
+				break
+		except:
+			break
+	IsTrue = 0
+	for Lines in Stack:
+		if ('elif' in Lines or 'else' in Lines) and IsTrue == 1:
+			break
+		elif 'if' in Lines or 'elif' in Lines:
+			Expression = Lines[2:Lines.find(":")] if 'if' in Lines else Lines[5:Lines.find(":")]
+			Expression = eval(ChangeVariables(Expression))
+			#print("exp =",Expression)
+			if Expression == True:
+				IsTrue = 1
+		elif 'else' in Lines:
+			IsTrue = 1
+		elif IsTrue == 1:
+			Run_Code(Lines)
 
-#Lines = "a = [1,2,[3,4]]"
-#print(">>>",end='')
-while True:
-	Lines = input(">>>")
+
+def Run_Code(Lines):
 	if Lines == "exit()":
-		break
-	if "=" in Lines and not "==" in Lines:
+		return 1
+	if Lines[:2] == 'if':
+		Branch(Lines)
+	elif "=" in Lines and not "==" in Lines:
 		Assign(Lines)
 	elif Lines in Variables:
 		print("A",Variables[Lines])
@@ -55,5 +80,16 @@ while True:
 		print(eval(Lines))
 	else:
 		print("B",eval(Lines))
+	return 0
+#Lines = "a = [1,2,[3,4]]"
+#print(">>>",end='')
+IsEnd = 0
+while not IsEnd:
+	Lines = input(">>>")
+	Lines = Lines.strip()
+	IsEnd = Run_Code(Lines)
 	print(Variables)
 	#Lines = input(">>>")
+
+
+
